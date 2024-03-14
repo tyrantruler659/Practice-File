94dd84a2eb987e70aa1e03c20080c4c4091ccfb2
tyrantruler659 <149674943+tyrantruler659@users.noreply.github.com>
2024-03-14 16:23:45 +0800

    Delete Sam edward.py

--- Sam edward.py
+++
@@ -1,9 +0,0 @@
-name = input("what is your name?")
-if name == "Tony":
-    print("welcome Iron")
-elif name == "Sam":
-    print("welcome new captain America")
-elif name == "Edward":
-    print("welcome King!")
-else:
-    print("I dont Know you!")
